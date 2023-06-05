c6f7171870544eac7cd4bc05800171c30f680f99
rabiebn <rabie.bnmr@gmail.com>
2023-06-05 15:06:37 -0700

    first commit of task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,14 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>/*???*/
+#include "lists.h"
 
 /**
- * check_cycle - checks if a listint_s singly linked list a cycle
- * @list: head of the listint_s singly linked list ????
+ * check_cycle - checks if a listint_t singly linked list a cycle
+ * @list: head of the listint_t singly linked list ????
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *hare, *tortoise;
 
+
+	if (list == NULL)
+		return (0);
+
+	tortoise = list;
+	hare = list->next;
+
+	if (hare == NULL)
+		return (0);
+
+	while (tortoise != hare)
+	{
+		if (hare->next == NULL || hare->next->next == NULL)
+			return (0);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return (1);
 }
