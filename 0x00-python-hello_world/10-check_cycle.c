a72691d8e8f5f6f03c91fc25dc30acbaf321a269
rabiebn <rabie.bnmr@gmail.com>
2023-06-05 11:59:44 -0700

    First commit 0x00 python hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>/*???*/
+
+/**
+ * check_cycle - checks if a listint_s singly linked list a cycle
+ * @list: head of the listint_s singly linked list ????
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+
+}
