bb9a1422e843e254e1cc2fa13f56c7cf04d520cb
rabiebn <rabie.bnmr@gmail.com>
2023-06-08 15:23:26 -0700

    2: corrected the plural/singular;3: pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,14 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv)
 
-    print("{} arguments".format((argc - 1)), end="")
+    print("{} argument".format((argc - 1)), end="")
 
     if argc == 1:
         print(".")
     else:
-        print(":")
         for i in range(1, argc):
+            if i == 1:
+                print(":")
+            else:
+                print("s:")
             print("{}: {}".format(i, argv[i]))
