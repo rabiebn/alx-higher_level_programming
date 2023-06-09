0fe7d4ddea94181db838c909259f64fd02d4dcff
rabiebn <rabie.bnmr@gmail.com>
2023-06-08 18:17:49 -0700

    103: first commit

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from fast_alphabet_103 import loop_AZ
+loop_AZ()
---
+++ 0x02-python-import_modules/fast_alphabet_103.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def loop_AZ():
+    for c in range(65, 91):
+        print("{}".format(chr(c)), end="")
+    print("")
