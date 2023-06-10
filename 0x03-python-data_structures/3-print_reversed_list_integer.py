47e090b92f3035c610a118d9e1d1fe4b3abc6bbc
rabiebn <rabie.bnmr@gmail.com>
2023-06-10 10:31:19 -0700

    3, 6: {:d}

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 def print_reversed_list_integer(my_list=[]):
     rev = -len(my_list)
     for i in range(-1, r - 1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,6 +7,6 @@ def print_matrix_integer(matrix=[[]]):
         for i in matrix:
             for j in range(len(i)):
                 if j == len(i) - 1:
-                    print("{}".format(i[len(i) - 1]))
+                    print("{:d}".format(i[len(i) - 1]))
                 else:
-                    print("{}".format(i[j]), end=" ")
+                    print("{:d}".format(i[j]), end=" ")
