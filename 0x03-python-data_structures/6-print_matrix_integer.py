2c4c0ee9f4e845dccee993a1c52726cb4e1b8c0c
rabiebn <rabie.bnmr@gmail.com>
2023-06-10 11:09:03 -0700

    6: checked if matrix is None

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1:
+    if not matrix:
+        exit(0)
+    elif len(matrix) == 1 and len(matrix[0]) == 0:
         print("")
     else:
         for i in matrix:
