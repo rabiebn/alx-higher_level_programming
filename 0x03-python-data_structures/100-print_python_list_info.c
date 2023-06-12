58720433342750b92c3fcc0565ad7519498b1789
rabiebn <rabie.bnmr@gmail.com>
2023-06-12 22:59:59 +0000

    100: CPython under the hood

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p:
+ *
+ * Return: void.
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, allocated, i;
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Pyhthon List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %ld: %s\n", i,
+				(Py_TYPE(PyList_GetItem(p, i)))->tp_name);
+	}
+}
