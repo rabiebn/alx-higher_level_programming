6a5188b13b7f99f1c0a07dbd0fad448c3eaac186
rabiebn <rabie.bnmr@gmail.com>
2023-06-14 15:23:42 -0700

    101: restored the orginal var name

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-def square_matrix_map(m=[]):
-    return list(map(lambda x: list(map(lambda i: x[i]**2, range(len(x)))), m))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda i: x[i]**2, range(len(x)))), matrix))
