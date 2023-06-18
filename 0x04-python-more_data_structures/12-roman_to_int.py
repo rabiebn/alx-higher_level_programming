67cde73b7293eb62a9c54f60648ddc9a5ac74d4f
rabiebn <rabie.bnmr@gmail.com>
2023-06-18 08:47:58 -0700

    12: got rid of reversing the string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,59 +5,59 @@ def roman_to_int(roman_string):
         return 0
 
     thousands = {'M': 1000,
-            'MM': 2000,
-            'MMM': 3000
-            }
+                 'MM': 2000,
+                 'MMM': 3000}
     hundreds = {'C': 100,
-            'CC': 200,
-            'CCC': 300,
-            'CD': 400,
-            'D': 500,
-            'DC': 600,
-            'DCC': 700,
-            'DCCC': 800,
-            'CM': 900
-            }
+                'CC': 200,
+                'CCC': 300,
+                'CD': 400,
+                'D': 500,
+                'DC': 600,
+                'DCC': 700,
+                'DCCC': 800,
+                'CM': 900}
     tens = {'X': 10,
             'XX': 20,
-            'XXX':30,
+            'XXX': 30,
             'XL': 40,
             'L': 50,
             'LX': 60,
             'LXX': 70,
             'LXXX': 80,
-            'XC': 90
-            }
+            'XC': 90}
     units = {'I': 1,
-            'II': 2,
-            'III': 3,
-            'IV': 4,
-            'V': 5,
-            'VI': 6,
-            'VII': 7,
-            'VIII': 8,
-            'IX': 9
-            }
-    roman_int = 0
-    rev_roman = roman_string[::-1]
-    idx = 0 
-    while idx < len(roman_string):
-        if (len(roman_string) - idx) < 4:
-            idx = len(roman_string)
-        else:
-            idx += 4
-        section = rev_roman[:idx]
+             'II': 2,
+             'III': 3,
+             'IV': 4,
+             'V': 5,
+             'VI': 6,
+             'VII': 7,
+             'VIII': 8,
+             'IX': 9}
+    convert = [thousands, hundreds, tens, units]
+    start = 0
+    finnish = 0
+    roman_int = found = 0  # found will help us to check the previous sum
+    if len(roman_string) - start >= 4:
+        finnish += 4
+    else:
+        finnish += len(roman_string) - start
 
-        if section in list(units.keys()):
-            roman_int += units[section]
-        elif section in list(tens.keys()):
-            roman_int += tens[section]
-        elif section in list(hundreds.keys()):
-            roman_int += hundreds[section]
-        elif section in list(thousands.keys()):
-            roman_int += thousands[section]
+    while start < len(roman_string):
+        section = roman_string[start: finnish]
+        for dict in convert:
+            if section in list(dict.keys()):
+                roman_int += dict[section]
+                start = finnish
+                if len(roman_string) - start >= 4:
+                    finnish += 4
+                else:
+                    finnish += len(roman_string) - start
+                break
+        if found == roman_int:  # checks if it found a value of section
+            finnish -= 1
         else:
-            idx -= 1
+            found = roman_int
             continue
 
     return roman_int
