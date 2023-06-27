2780a10c84f562c80c0641bf82ed38a531f60c6f
rabiebn <rabie.bnmr@gmail.com>
2023-06-27 16:19:08 -0700

    0, 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """Return Square definition"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class Square:
+    """Returns a definition of a square"""
+    def __init__(self, size):
+        self.__size = size
