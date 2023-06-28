db976a45e4428783cb0337fe622458f0c93d11fc
rabiebn <rabie.bnmr@gmail.com>
2023-06-28 05:04:55 -0700

    1: added documentation for __init__

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Square Module Docs
+Author: Rabia Benmer
+"""
+
 
 class Square:
-    """Returns a definition of a square"""
+    """Return a Square object"""
+    
     def __init__(self, size):
+         """
+         Initialize Square attributes.
+         
+         Parameters:
+         size (int):The size of the Square (Private instnce attribute).
+         """
+
         self.__size = size
