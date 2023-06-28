b3b1f50c7e7e7239d065be0dfa7c0f31ec124c09
rabiebn <rabie.bnmr@gmail.com>
2023-06-28 14:19:20 -0700

    4: Safe dividing two list elements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    div = 0
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            div = 0
+            print("out of range")
+        except ZeroDivisionError:
+            div = 0
+            print("division by 0")
+        except TypeError:
+            div = 0
+            print("wrong type")
+        finally:
+            div_list.append(div)
 
+    return div_list
