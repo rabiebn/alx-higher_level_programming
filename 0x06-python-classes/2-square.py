7792d9d9a9b9f2d00480efa298485bff03e19485
rabiebn <rabie.bnmr@gmail.com>
2023-06-28 15:01:03 -0700

    2: size attribute shoud be a positive int

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private attribute size
+Author: Rabia Benmer
+"""
+
+
+class Square:
+    """
+    Class Square definition
+
+    Args:
+        size (int): size of a square side
+    """
+    def __init__(self, size=0):
+        """
+        Initialize Square.
+
+        Attributes:
+            size (int): size of square side.
+        """
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
