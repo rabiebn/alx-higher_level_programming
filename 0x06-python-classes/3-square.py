4fd8658e7ade08ca6b408856ec67e8f87e04b66b
rabiebn <rabie.bnmr@gmail.com>
2023-06-28 15:09:55 -0700

    3: pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -26,5 +26,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
+        """Compute Square Area"""
         return self.__size ** 2
