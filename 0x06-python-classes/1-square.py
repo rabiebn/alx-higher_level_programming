0e83bf8ec3a6ffac02e2d66640c2d79f717dd58f
rabiebn <rabie.bnmr@gmail.com>
2023-06-28 05:17:05 -0700

    1: pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Square Module Docs
+"""
+Module 1-square
+Defines class Square with private attribute size
 Author: Rabia Benmer
 """
 
 
 class Square:
-    """Return a Square object"""
-    
-    def __init__(self, size):
-         """
-         Initialize Square attributes.
-         
-         Parameters:
-         size (int):The size of the Square (Private instnce attribute).
-         """
+    """
+    Class Square definition
 
+    Args:
+        size (int): size of a square side
+    """
+    def __init__(self, size):
+        """
+        Initialize Square.
+
+        Attributes:
+            size (int): size of square side.
+        """
         self.__size = size
