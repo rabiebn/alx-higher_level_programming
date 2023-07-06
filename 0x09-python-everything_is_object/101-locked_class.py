0c248bc2390b0faf26707baeaee37059b4188567
rabiebn <rabie.bnmr@gmail.com>
2023-07-06 11:35:50 -0700

    101: used __slots instead of __setattr__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,9 +10,4 @@ class LockedClass:
     prevents the user from dynamically creating attributes
     other than 'first_name'
     """
-
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("object has no attribute '{}'".format(name))
+    __slots__ = ["first_name"]
