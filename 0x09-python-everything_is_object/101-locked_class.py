4b77b342436c2a8c387e5194f563886447fb80bf
rabiebn <rabie.bnmr@gmail.com>
2023-07-06 11:31:37 -0700

    101: Module Documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Module for class LockedClass that prevents dynamic creation of atributes
+"""
+
 
 class LockedClass:
+    """
+    LockedClass class has no class or object attributes,
+    prevents the user from dynamically creating attributes
+    other than 'first_name'
+    """
+
     def __setattr__(self, name, value):
         if name == "first_name":
             self.__dict__[name] = value
