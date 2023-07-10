6b6e5e90fa3024f11f7b86a2852f5e4406c0c7c4
rabiebn <rabie.bnmr@gmail.com>
2023-07-10 11:21:41 -0700

    0x0A-python-inheritance

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Module 4-square
+Defines class Square with private attribute size
+Author: Rabia Benmer
+"""
+
+
+class Square:
+    """
+    Class Square definition
+
+    Args:
+        size (int): size of a square side
+
+    Functions:
+        __init__(self, size=0)
+        size(self)
+        size(self, value)
+    """
+    def __init__(self, size=0):
+        """
+        Initialize Square.
+
+        Attributes:
+            size (int): size of square side.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value (if int and >= 0)
+        """
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Compute Square Area
+
+        Return:
+            area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print Square"""
+        side = self.size
+        if side == 0:
+            print("")
+
+        for i in range(side):
+            for j in range(side - 1):
+                print("#", end='')
+            print("#")
