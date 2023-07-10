3d47bf887cb347ff98908b171c22b4dfd2ff813b
rabiebn <rabie.bnmr@gmail.com>
2023-07-10 16:16:12 -0700

    6: Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+6-base_geometry Module
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry Class
+    """
+    def area(self):
+        """
+        Raises an Exception
+        """
+        raise Exception("area() is not implemented")
