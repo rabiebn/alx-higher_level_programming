973e3540b707d1abb22c54861b48491c476dbc2a
rabiebn <rabie.bnmr@gmail.com>
2023-07-11 11:41:30 -0700

    0: Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-read_file Module has 1 function:
+    read_file()
+"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding='UTF-8') as a_file:
+        for line in a_file:
+            print(line, end='')
