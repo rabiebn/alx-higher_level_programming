deb278c7166d2f27a80f598eeae00822d07639be
rabiebn <rabie.bnmr@gmail.com>
2023-07-11 11:43:32 -0700

    0: Added open mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, encoding='UTF-8') as a_file:
+    with open(filename, mode='r', encoding='UTF-8') as a_file:
         for line in a_file:
             print(line, end='')
