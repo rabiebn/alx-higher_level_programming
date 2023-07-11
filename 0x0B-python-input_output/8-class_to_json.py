5120ac4b36dae543bd3ec3f3803467771c3fd793
rabiebn <rabie.bnmr@gmail.com>
2023-07-11 15:10:09 -0700

    8: pycodestyle

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,16 +3,17 @@
 8-class_to_json Module
 """
 
+
 def class_to_json(obj):
-        """
-        Returns the dictionary description with simple data structure
-        (list, dictionary, string, integer and boolean)
-        for JSON serialization of an object.
+    """
+    Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object.
 
-        Args:
-            obj (Any): Instance of a Class.
+    Args:
+        obj (Any): Instance of a Class.
 
-        Return:
-            dict description for JSON serialization of 'obj'.
+    Return:
+        dict description for JSON serialization of 'obj'.
         """
-        return obj.__dict__
+    return obj.__dict__
