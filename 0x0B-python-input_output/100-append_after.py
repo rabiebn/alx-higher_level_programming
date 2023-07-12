19671eef2832849ace7326b70b9f50f7efffbb71
rabiebn <rabie.bnmr@gmail.com>
2023-07-12 12:22:06 -0700

    100: Fixed appending to the end of the file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -13,9 +13,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, mode='r', encoding='UTF-8') as my_file:
         lines = my_file.readlines()
 
-    for i in range(len(lines)):
-        if search_string in lines[i]:
-            lines.insert(i + 1, new_string)
+    for i in lines:
+        if search_string in i:
+            lines.insert(lines.index(i) + 1, new_string)
 
     with open(filename, mode='w', encoding='UTF-8') as my_file:
         my_file.writelines(lines)
