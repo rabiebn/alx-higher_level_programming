5b52c3813aa22461e8e4741adde42a70e7a50515
rabiebn <rabie.bnmr@gmail.com>
2023-07-12 09:48:06 -0700

    10: Initial commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+10-square Module.
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class.
+    """
+
+    def __init__(self, size):
+        """
+        init
+
+        Args:
+            size (int): integer > 0.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Area of the Square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        str
+        """
+        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
