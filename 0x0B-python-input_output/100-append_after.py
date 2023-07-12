e1b2a2fa2ab068a6a803db451000c2d43173eb1d
rabiebn <rabie.bnmr@gmail.com>
2023-07-12 12:39:29 -0700

    100: Abonded dealing with list of lines

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,13 +10,12 @@ def append_after(filename="", search_string="", new_string=""):
     a specific string.
     """
 
+    new_file = ""
     with open(filename, mode='r', encoding='UTF-8') as my_file:
-        file_lines = my_file.readlines()
-
-    lines = file_lines[:]
-    for i in file_lines:
-        if search_string in i:
-            lines.insert(lines.index(i) + 1, new_string)
+        for line in my_file:
+            new_file += line
+            if search_string in line:
+                new_file += search_string
 
     with open(filename, mode='w', encoding='UTF-8') as my_file:
         my_file.writelines(lines)
