7eecfb6361b41503899d0e8c1470478bbaa7929c
rabiebn <rabie.bnmr@gmail.com>
2023-07-17 13:38:49 -0700

    Added update() to Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,23 @@ class Square(Rectangle):
         """Str"""
         return "[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__, self.id, self.x, self.y, self.height)
+    def update(self, *args, **kwargs):
+        """Update attributes"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if "id" in kwargs.keys():
+                self.id = kwargs["id"]
+            if "size" in kwargs.keys():
+                self.size = kwargs["size"]
+            if "x" in kwargs.keys():
+                self.x = kwargs["x"]
+            if "y" in kwargs.keys():
+                self.y = kwargs["y"]
