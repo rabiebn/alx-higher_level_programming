ad8c77854215ed88f0b8d987b604ae6ea8797510
rabiebn <rabie.bnmr@gmail.com>
2023-07-17 11:38:40 -0700

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Init
@@ -33,6 +34,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """width Setter"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = width
 
     @property
@@ -43,6 +49,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """height Setter"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -53,6 +63,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """x Setter"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -63,4 +77,25 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y Setter"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """Calculating Area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Display the Rectangle with #"""
+
+        for j in range(self.__height):
+            for i in range(self.__width - 1):
+                print("#", end='')
+            print("#")
+
+    def __str__(self):
+        """str"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x,  self.__y, self.__width, self.__height)
