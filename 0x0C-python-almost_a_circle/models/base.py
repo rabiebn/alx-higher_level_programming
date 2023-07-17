c77c941a71c794b1c027757bf086fe354c917c96
rabiebn <rabie.bnmr@gmail.com>
2023-07-17 14:40:32 -0700

    added save_to_file() to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,12 @@ class Base():
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of 'list_objs' to a file"""
+
+        with open("{}.json".format(cls.__name__), "w",
+                  encoding='UTF-8') as file:
+            file.write(cls.to_json_string(
+                list(d.to_dictionary() for d in list_objs)))
