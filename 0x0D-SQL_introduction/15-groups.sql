7c4e519765cb21639cb3b21c1a6b420c2e0652cb
rabiebn <rabie.bnmr@gmail.com>
2023-08-15 17:42:21 -0700

    15-16: Initial commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- Lists the number of records with the same score in the table 'second_table' in DB 'hbtn_0c_0'.
+
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
+
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists score and name of all records of the table 'second_table'.
+
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
+
