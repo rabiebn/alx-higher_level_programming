68a9eecb858d7b014d6c9316d9e961613b2d6201
rabiebn <rabie.bnmr@gmail.com>
2023-09-15 14:14:00 -0700

    0-1: Initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,\
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states')
+    states = cur.fetchall()
+    for row in states:
+        if row[1].startswith('N'):
+            print(row)
+    cur.close()
+    db.close()
+
