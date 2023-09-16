afaf8b1236848f13f7b479b037a2aac0ae25016c
rabiebn <rabie.bnmr@gmail.com>
2023-09-16 03:43:17 -0700

    2: Using cursor in safe mode (with)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,13 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(
-                sys.argv[4]))
-    states = cur.fetchall()
+    with db.cursor() as cur:
+        cur.execute("""
+                    SELECT * FROM states
+                    WHERE name = '{}'
+                    ORDER BY id
+                    """.format(sys.argv[4]))
+        states = cur.fetchall()
     for row in states:
         print(row)
     cur.close()
