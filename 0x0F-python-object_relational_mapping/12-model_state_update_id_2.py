4669d02ac1289b08a5283cb3e2dbe9e9bc1cef08
rabiebn <rabie.bnmr@gmail.com>
2023-09-17 10:37:28 -0700

    12: Initial commit

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Adds a State object to states table in DB hbtn_0e_6_usa.
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, exc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id_state == 2).update()\
+        .values({'name': 'New Mexico'})
+    session.commit()
+
+    """
+    # 2nd method to update an entry:
+    state = session.query(State).filter(State.id_state == 2).one()
+    state.name = 'New Mexico'
+    session.commit()
+    """
