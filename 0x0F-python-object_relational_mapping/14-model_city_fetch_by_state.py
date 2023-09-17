0f57cbc5460f8678140137ec7b6129d9a92cd21e
rabiebn <rabie.bnmr@gmail.com>
2023-09-17 13:35:52 -0700

    14: Initial commit

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Prints all objects from DB hbtn_0e_14_usa.
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine, exc
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    city_state = session.query(State, City).join(State).order_by(
+            City.id_city).all()
+    for row in city_state:
+        print("{}: ({}) {}".format(row[0].name, row[1].id_city, row[1].name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+"""
+model_city defines City class.
+"""
+
+
+class City(Base):
+    """
+    City class
+    """
+    __tablename__ = 'cities'
+    id_city = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey("states.id"))
+
+    def __init__(self, name):
+        """
+        Initialize City objs
+        """
+        self.name = name
