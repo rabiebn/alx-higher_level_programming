db0beb05b18d86c51441487ea569de8ec86ae119
rabiebn <rabie.bnmr@gmail.com>
2023-09-17 05:36:49 -0700

    11: Initial commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Adds a State object to states table in DB hbtn_0e_6_usa.
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, exc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    state_la = State('Louisiana')
+    session.add(state_la)
+    print(state_la.id_state)
+    session.commit()
