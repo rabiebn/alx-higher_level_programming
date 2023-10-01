38f89a861173ebacb23e0421cd103a0261b575f1
rabiebn <rabie.bnmr@gmail.com>
2023-10-01 14:10:18 -0700

    11: semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const { argv } = require('process');
 if (argv.length <= 3) { console.log('0'); } else {
-	const arr = argv.slice(2);
-	arr.sort((x, y) => (parseInt(y) - parseInt(x)));
-	console.log(arr[1]);
+  const arr = argv.slice(2);
+  arr.sort((x, y) => (parseInt(y) - parseInt(x)));
+  console.log(arr[1]);
 }
