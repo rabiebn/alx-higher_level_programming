b254d74057274290689c0ef4c54941e215a0915e
rabiebn <rabie.bnmr@gmail.com>
2023-10-01 08:16:35 -0700

    100: Got itgit add .!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+given repo and owner name as argvs, uses Github API to list last 10 commits
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            argv[2], argv[1])
+    r = requests.get(url)
+    list_commits = r.json()
+    for commit in list_commits[0:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
